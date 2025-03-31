7925d10ac1afecf79ef3253543fbbe463adf7a2b
lionDev <liosilman02@gmail.com>
2025-03-31 20:32:43 -0300

    reset-pass fix

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const app = express()
 
 // Basic configuration
 app.use(cors({
-  origin: "https://utn-2025-fe-dpl.vercel.app" || "http://localhost:5173",
+  origin: "https://utn-2025-fe-dpl.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }))
