a555d2190ab3d5e13463fbd1f4a6075842ff3969
lionDev <liosilman02@gmail.com>
2025-03-31 17:55:20 -0300

    mongodb fix

--- src/config/mongodb.config.js
+++ src/config/mongodb.config.js
@@ -3,14 +3,17 @@ import { ENVIROMENT } from "./enviroment.config.js"
 
 const connectToMongoDB = async () => {
   try {
- 
-    const response = await mongoose.connect(ENVIROMENT.MONGODB_URL)
+    const response = await mongoose.connect(ENVIROMENT.MONGODB_URL, {
+      connectTimeoutMS: 60000,  // Aumenta el tiempo de conexión a 60 segundos
+      socketTimeoutMS: 60000,   // Aumenta el tiempo de espera en los sockets
+      serverSelectionTimeoutMS: 60000, // Aumenta el tiempo para la selección del servidor
+    })
     
     console.log("✅ Conectado a MongoDB // Base de datos:", response.connection.name)
     return response
   } catch (error) {
     console.error("❌ Error al conectar a MongoDB:", error.message)
-    // No lanzamos el error para permitir que el servidor inicie aunque falle la conexións
+    // No lanzamos el error para permitir que el servidor inicie aunque falle la conexión
   }
 }
 
@@ -18,4 +21,3 @@ const connectToMongoDB = async () => {
 connectToMongoDB()
 
 export default mongoose
-
