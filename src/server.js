6cc8e8d239037db5f81220f063914bd875a5802f
lionDev <liosilman02@gmail.com>
2025-03-31 19:32:53 -0300

    reset-pass fix

--- src/server.js
+++ src/server.js
@@ -14,10 +14,7 @@ import channelRouter from "./routes/channel.router.js"
 const app = express()
 
 // Basic configuration
-app.use(cors({
-  origin: 'https://utn-2025-fe-dpl.vercel.app',
-  credentials: true
-}))
+app.use(cors({}))
 app.use(express.json({ limit: "5mb" }))
 app.use(express.urlencoded({ extended: true }))
 
