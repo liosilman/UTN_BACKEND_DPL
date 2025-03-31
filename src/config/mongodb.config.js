55f87c2ec04ca03ad1d935fab5c6ed4fb3238ac4
lionDev <liosilman02@gmail.com>
2025-03-31 18:04:14 -0300

    mongodb fix

--- src/config/mongodb.config.js
+++ src/config/mongodb.config.js
@@ -4,9 +4,7 @@ import { ENVIROMENT } from "./enviroment.config.js"
 const connectToMongoDB = async () => {
   try {
     const response = await mongoose.connect(ENVIROMENT.MONGODB_URL, {
-      connectTimeoutMS: 60000,  // Aumenta el tiempo de conexión a 60 segundos
-      socketTimeoutMS: 60000,   // Aumenta el tiempo de espera en los sockets
-      serverSelectionTimeoutMS: 60000, // Aumenta el tiempo para la selección del servidor
+
     })
     
     console.log("✅ Conectado a MongoDB // Base de datos:", response.connection.name)
