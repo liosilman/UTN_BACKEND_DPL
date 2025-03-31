959b44058567304a9e895ecf4949551888ea40d0
lionDev <liosilman02@gmail.com>
2025-03-31 17:35:03 -0300

    BD Fix

--- src/config/mongodb.config.js
+++ src/config/mongodb.config.js
@@ -5,12 +5,12 @@ const connectToMongoDB = async () => {
   try {
  
     const response = await mongoose.connect(ENVIROMENT.MONGODB_URL)
+    
     console.log("✅ Conectado a MongoDB // Base de datos:", response.connection.name)
     return response
   } catch (error) {
     console.error("❌ Error al conectar a MongoDB:", error.message)
-    // No lanzamos el error para permitir que el servidor inicie aunque falle la conexión
-    // Esto es útil para Vercel que puede reintentar la conexión
+    // No lanzamos el error para permitir que el servidor inicie aunque falle la conexións
   }
 }
 
